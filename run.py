38185723401f2635b00f972d79171c07a64f84e2
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-04-22 12:35:08 +0000

    about us is completed

--- run.py
+++ run.py
@@ -19,6 +19,20 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+       data = json.load(json_data)
+       for obj in data:
+         if obj["url"] == member_name:
+            member = obj
+
+    return render_template("member.html", member=member) 
+    # This first 'member' is the variable name being passed through into our html file.
+    # The second 'member' is the member object we created above on line 24.
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
