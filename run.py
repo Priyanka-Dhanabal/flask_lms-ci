1662fdfd97e50a94d9b63e0918a70294519700c9
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-04-22 11:58:51 +0000

    All about creating json file and minimising the code repeation

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template # render_template to import files into the website
 
 
@@ -12,17 +13,20 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data: # Python open the JSON file in order to read it. This is called a 'with' block.
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 
